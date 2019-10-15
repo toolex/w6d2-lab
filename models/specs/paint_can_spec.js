530dd1007f0e5597e8adb7f4d88757f4b5365be8
Iain Tooley <iaintooley1@gmail.com>
2019-10-15 16:01:37 +0100

    adds test for fullness of the paint can

--- models/specs/paint_can_spec.js
+++ models/specs/paint_can_spec.js
@@ -6,12 +6,17 @@ describe("Paint Can", function () {
   let paint_can;
 
   beforeEach(function(){
-    paint_can = new PaintCan(20, true)
+    paint_can = new PaintCan(20, 0)
   });
 
   it("should have a number of litres of paint", function(){
     const actual = paint_can.capacity
     assert.strictEqual(actual, 20)
+  });
+
+  it("should show the paint can is empty", function(){
+    const actual = paint_can.fullness
+    assert.strictEqual(actual, 0)
   });
 
 });
